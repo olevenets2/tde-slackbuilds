06db7d713e90d7a709b302351f57cb4b97d67cc3
Thorn Inurcide <thorn@darkstar.blackhole>
2015-08-15 05:03:13 -0500

    updates

---
+++ build-core.sh
@@ -0,0 +1,77 @@
+#!/bin/sh
+
+# Copyright 2012  Patrick J. Volkerding, Sebeka, Minnesota, USA
+# All rights reserved.
+#
+# Copyright 2014 Willy Sudiarto Raharjo <willysr@slackware-id.org>
+# All rights reserved.
+#
+# Copyright 2015 Thorn Inurcide
+#
+# Based on the xfce-build-all.sh script by Patrick J. Volkerding
+#
+# Redistribution and use of this script, with or without modification, is
+# permitted provided that the following conditions are met:
+#
+# 1. Redistributions of this script must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#
+#  THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#  WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+#  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO
+#  EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+#  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+#  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+#  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# These need set here:
+INST=1
+TMP=${TMP:-/tmp}
+ROOT=$(pwd)
+
+# Loop for all packages
+for dir in \
+  Core/tdelibs \
+  Core/tdebase \
+  Core/tdeutils \
+  Core/tdemultimedia \
+  Core/tdenetwork \
+  Core/tdeartwork \
+  Core/tdegraphics \
+  Core/tdepim \
+  Core/tdesdk \
+  Core/tdevelop \
+  Core/tdeaddons \
+  Core/tdetoys \
+  Core/tdewebdev \
+  ; do
+  # Get the package name
+  package=$(echo $dir | cut -f2- -d /)
+
+  # Change to package directory
+  cd $ROOT/$dir || exit 1
+
+  # Get the version
+  version=$(cat ${package}.SlackBuild | grep "VERSION:" | head -n1 | cut -d "-" -f2 | rev | cut -c 2- | rev)
+
+  # Get the build
+  build=$(cat ${package}.SlackBuild | grep "BUILD:" | cut -d "-" -f2 | rev | cut -c 2- | rev)
+
+  # The real build starts here
+  sh ${package}.SlackBuild || exit 1
+  if [ "$INST" = "1" ]; then
+    PACKAGE=`ls --color=never $TMP/${package}-${version}-*-${build}*.txz`
+    if [ -f "$PACKAGE" ]; then
+      upgradepkg --install-new --reinstall "$PACKAGE"
+    else
+      echo "Error:  package to upgrade "$PACKAGE" not found in $TMP"
+      exit 1
+    fi
+  fi
+
+  # back to original directory
+  cd $ROOT
+done
